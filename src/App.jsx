206d4860c549205c6ed92c7c63c6ebde069b910d
drishti <drishtisingla84@gmail.com>
2024-11-29 01:25:58 +0530

    Save

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/nav";
 import Sign from "./components/SignIn";
-//import Profile from "./components/profile"; // Assuming you create this component
 import Profile from "./components/profile";
-import { green } from "@mui/material/colors";
-
+import Job from "./components/Job";
 function App() {
   return (
     <Router>
@@ -13,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Sign but="Register" a="Login" text="Already have an account?">Sign Up</Sign>} />
         <Route path="/profile" element={<Profile value={80} color="green" userId={1}/>} />
+        <Route path="/job" element={<Job/>} />
+          
       </Routes>
     </Router>
   );
