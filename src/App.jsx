093043a0bc48d315fcd03701ef5cb62f24d9965f
Achin-Agarwal <achinagarwal9044@gmail.com>
2024-11-29 23:54:43 +0530

    joblist and userlist added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Sign from "./components/SignIn";
 import Profile from "./components/profile";
 import { green } from "@mui/material/colors";
 import Experts from "./Pages/Experts";
+import Joblist from "./components/joblist";
+import node from "./assets/node.jpg";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Route path="/" element={<Sign but="Register" a="Login" text="Already have an account?">Sign Up</Sign>} />
         <Route path="/profile" element={<Profile value={80} color="green" userId={1}/>} />
         <Route path="/experts" element={<Experts />} />
+        <Route path="/list" element={<Joblist imageSrc={node} job={"Node.js Developer"} application={"Applications: 101"} open={"Opened 2 Days Ago"}/>}/>
       </Routes>
     </Router>
   );
---
+++ src/components/Joblist.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./joblist.css";
+import Heading from "./Heading";
+
+const Joblist = ({ imageSrc,job,application,open }) => {
+  return (
+    <div className="container">
+      <div className="image-container">
+        <img src={imageSrc} alt="Job Image" className="image" />
+      </div>
+      <div className="text">
+        <Heading fontSize="20px" fontWeight="600" className="job">{job}</Heading>
+        <Heading fontSize="13px" fontWeight="400" className="application">{application}</Heading>
+        <Heading fontSize="10px" fontWeight="400" className="open">{open}</Heading>
+      </div>
+      <div className="suffix">
+        hey
+      </div>
+    </div>
+  );
+};
+
+export default Joblist;
