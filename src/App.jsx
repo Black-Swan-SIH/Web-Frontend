0b6cef732e5b9a5c2d5ab4103bf35f6bf77fa2da
Achin-Agarwal <achinagarwal9044@gmail.com>
2024-11-30 11:23:22 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Sign from "./components/SignIn";
 import Profile from "./components/profile";
 import { green } from "@mui/material/colors";
 import Experts from "./Pages/Experts";
+import Job from "./components/Job";
+import Joblist from "./components/Joblist";
+import node from "./assets/node.jpg";
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
         <Route path="/" element={<Sign but="Register" a="Login" text="Already have an account?">Sign Up</Sign>} />
         <Route path="/profile" element={<Profile value={80} color="green" userId={1}/>} />
         <Route path="/experts" element={<Experts />} />
+        <Route path="/job" element={<Job />} />
+        <Route path="/userlist" element={<Joblist imageSrc={node} jobs={"Node.js Developer"} application={"Applications: 101"} open={"Opened 2 Days Ago"}/>}/>
       </Routes>
     </Router>
   );
--- src/components/Joblist.jsx
+++ src/components/Joblist.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import "./joblist.css";
+import "./Joblist.css";
 import Heading from "./Heading";
 
-const Joblist = ({ imageSrc,job,application,open }) => {
+const Joblist = ({ imageSrc,jobs,application,open }) => {
   return (
     <div className="container">
       <div className="image-container">
         <img src={imageSrc} alt="Job Image" className="image" />
       </div>
       <div className="text">
-        <Heading fontSize="20px" fontWeight="600" className="job">{job}</Heading>
+        <Heading fontSize="20px" fontWeight="600" className="job">{jobs}</Heading>
         <Heading fontSize="13px" fontWeight="400" className="application">{application}</Heading>
         <Heading fontSize="10px" fontWeight="400" className="open">{open}</Heading>
       </div>
