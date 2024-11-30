2e95fbeed2d53162f46ceb3271c2d9eeffc10fb7
Achin-Agarwal <achinagarwal9044@gmail.com>
2024-11-30 18:11:16 +0530

    search box added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 import NavBar from "./components/nav";
 import Sign from "./components/SignIn";
 import Profile from "./components/profile";
@@ -7,6 +8,7 @@ import { green } from "@mui/material/colors";
 import Experts from "./Pages/Experts";
 import Job from "./components/Job";
 import Itemcount from "./components/Itemcount";
+import Candidatelist from "./Pages/Candidatelist";
 // import Joblist from "./components/Joblist";
 // import node from "./assets/node.jpg";
 
@@ -20,6 +22,7 @@ function App() {
         <Route path="/experts" element={<Experts />} />
         <Route path="/job" element={<Job />} />
         <Route path="/itemcount" element={<Itemcount head="Total Experts" value="11.26K"/>} />     
+        <Route path="/candidatelist" element={<Candidatelist />}/>
         {/* <Route path="/joblist" element={<Joblist imageSrc={node} jobs={"Node.js Developer"} application={"Applications: 101"} open={"Opened 2 Days Ago"}/>}/> */}
       </Routes>
     </Router>
